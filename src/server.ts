4cb5450143cbc87b686d06f4c5ff2afc75d7c314
Levy Gomes <levybusinessjr@gmail.com>
2022-12-26 14:22:24 -0400

    base criada

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import mainRoutes from './routes/rotas'
+
+dotenv.config()
+
+//Configurações do servidor e do template engine
+const server = express()
+
+server.set('view engine', 'mustache')
+server.set('views', path.join('__dirname', 'views'))
+server.engine('mustache', mustache())
+
+//essa linha de código é feita para que consigamos entregar arquivos estáticos no nosso servidor, como HTML, CSS, Javascript, imagens
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use(mainRoutes)
+
+server.use((req, res) => {
+  res.status(404).send('página não encontrada')
+})
+
+server.listen(process.env.PORT)
